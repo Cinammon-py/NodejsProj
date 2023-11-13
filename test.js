8544ccd94d20889be2763079869997bf0be5a476
Cinammon-py <102076657+Cinammon-py@users.noreply.github.com>
2023-11-13 11:34:09 +0200

    nnn

--- test.js
+++ test.js
@@ -6,3 +6,18 @@ const greet = (name) => {
 };
 
 greet(name);
+
+// Global Object, similar to window obj in JS
+console.log(global);
+
+// setTimeout method
+setTimeout(() => {
+  console.log('in the timeout');
+  clearInterval(int);
+}, 3000);
+const int = setInterval(() => {
+  console.log('in the Interval');
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
