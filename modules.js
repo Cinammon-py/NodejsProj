78c312a82965bba6b0605fe4dba51d6d04a25662
Cinammon-py <102076657+Cinammon-py@users.noreply.github.com>
2023-11-13 12:11:34 +0200

    Includes OS

--- modules.js
+++ modules.js
@@ -3,3 +3,11 @@ const xyz = require('./people');
 
 // using module to export content to the variable xyz from the people file
 console.log(xyz);
+
+// logged in a linear fashion
+console.log(xyz.people, xyz.ages);
+
+// OS module
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
