5d696aeddfe4cd8c66e0c19c5a7a85d3cf4932ce
Cinammon-py <102076657+Cinammon-py@users.noreply.github.com>
2023-11-20 08:59:58 +0200

    Monday commit

--- Server/server2.js
+++ Server/server2.js
@@ -6,8 +6,32 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
+  // set Header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views';
+  switch (req.url) {
+    case '/':
+      path += '/index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += '/about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us': //old URL to be redirected
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end;
+      break;
+    default:
+      path += '/404.html';
+      res.statusCode = 404;
+      break;
+  }
+
   // send an HTML File
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
